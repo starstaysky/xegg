e270040b2249b5dd4b9acc45d86a2a75489972ce
xushengju <xushengju@didichuxing.com>
2018-08-18 12:07:49 +0800

    add index

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,1 +1,9 @@
-const Controller = require("egg")
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller{
+    async index(){
+        this.ctx.body = "Hello fuxing";
+    }
+}
+
+module.exports = HomeController;
